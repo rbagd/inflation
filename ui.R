2dc1fd50bd9f5524dfc5d6f92ecf33ffbfe465d1
Rytis Bagdziunas <rytis.bagdziunas@uclouvain.be>
2014-05-27 13:59:25 +0200

    Data for May 2014.

--- ui.R
+++ ui.R
@@ -35,7 +35,7 @@ shinyUI(pageWithSidebar(
     selectInput(inputId = "window.start.month",
                 label = "",
                 choices = c(1:12),
-                selected = 4),
+                selected = 5),
     
     selectInput(inputId = "window.end.year",
                 label = "Jusqu'à",
@@ -45,7 +45,7 @@ shinyUI(pageWithSidebar(
     selectInput(inputId = "window.end.month",
                 label = "",
                 choices = c(1:12),
-                selected = 4),
+                selected = 5),
     
     checkboxInput(inputId = "weighted",
                   label = strong("Données pondérées"),
