f5f42b68fd62d35366520d20d74f9cbb6694a71c
Rytis Bagdziunas <mail@rbagd.eu>
2014-07-09 12:24:30 +0200

    Minor UI update.

--- ui.R
+++ ui.R
@@ -35,7 +35,7 @@ shinyUI(pageWithSidebar(
     selectInput(inputId = "window.start.month",
                 label = "",
                 choices = c(1:12),
-                selected = 5),
+                selected = 6),
     
     selectInput(inputId = "window.end.year",
                 label = "Jusqu'à",
@@ -45,7 +45,7 @@ shinyUI(pageWithSidebar(
     selectInput(inputId = "window.end.month",
                 label = "",
                 choices = c(1:12),
-                selected = 5),
+                selected = 6),
     
     checkboxInput(inputId = "weighted",
                   label = strong("Données pondérées"),
