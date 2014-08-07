50769e1841187d43dfaceab20e488d4e20585862
Rytis Bagdziunas <mail@rbagd.eu>
2014-08-07 13:49:03 +0200

    Data for July 2014.

--- ui.R
+++ ui.R
@@ -35,7 +35,7 @@ shinyUI(pageWithSidebar(
     selectInput(inputId = "window.start.month",
                 label = "",
                 choices = c(1:12),
-                selected = 6),
+                selected = 7),
     
     selectInput(inputId = "window.end.year",
                 label = "Jusqu'à",
@@ -45,7 +45,7 @@ shinyUI(pageWithSidebar(
     selectInput(inputId = "window.end.month",
                 label = "",
                 choices = c(1:12),
-                selected = 6),
+                selected = 7),
     
     checkboxInput(inputId = "weighted",
                   label = strong("Données pondérées"),
